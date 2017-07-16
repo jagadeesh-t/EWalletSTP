688ccfa4de0b94b388e89d355629ada178368432
Atul R <atulanand94@gmail.com>
2017-07-16 19:04:31 +0530

    Build script complete for android

--- scripts/android/decrypt-files.sh
+++ scripts/android/decrypt-files.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-echo "Decrypting keystore."
+echo "Decrypting keystores."
 
 openssl aes-256-cbc -k "$KEY_STORE_PASSWORD" -in scripts/android/keystore/debug.keystore.enc -d -a -out android/app/debug.keystore
+
+
+openssl aes-256-cbc -k "$KEY_STORE_PASSWORD" -in scripts/android/keystore/ewallet.keystore.enc -d -a -out android/app/ewallet.keystore
 
 echo "Decrypting firebase file."
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,13 @@
 echo "$DEVICE: Building"
 
 if [[ "$DEVICE" = "ANDROID" ]]; then
+
   echo $BUILD_NUMBER
-  cd android && ./gradlew clean assembleRelease && cd ..
+
+  cd android
+
+  ./gradlew clean assembleRelease -PVERSION_NUMBER=$BUILD_NUMBER -PMYAPP_RELEASE_STORE_FILE=ewallet.keystore -PMYAPP_RELEASE_KEY_ALIAS=com.ewalletSTP -PMYAPP_RELEASE_STORE_PASSWORD=$KEY_STORE_PASSWORD -PMYAPP_RELEASE_KEY_PASSWORD=$KEY_PASSWORD &&
+
+  cd ..
+
 fi
